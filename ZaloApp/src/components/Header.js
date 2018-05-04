5fe8c776c699668d3f06860b260165ebe4e0d762
Andy Ngo <andy.ngo@Jacks-Mac.local>
2018-05-03 21:09:03 -0700

    Update Status bar

--- ZaloApp/src/components/Header.js
+++ ZaloApp/src/components/Header.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import {
     AppRegistry, FlatList, StyleSheet, Text, View, Image, Alert,
     Platform, TouchableOpacity, Dimensions,
-    TextInput, Button
+    TextInput, Button, StatusBar
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
-
+import MyStatusBar from './MyStatusBar';
 export default class Header extends Component {
 
     constructor(props) {
@@ -16,14 +16,18 @@ export default class Header extends Component {
     goBack = () => {
         this.props.navigation.dispatch({ type: 'Navigation/BACK' });
     }
+    
 
     render() {
         return (
+           
             <LinearGradient 
                     start={{x: 1.0, y: 0.0}} end={{x: 0, y: 1}}
                     colors={['#25b8f7','#006FFD']}
                     style = { styles.container}>
+                     <MyStatusBar backgroundColor="#006FFD" barStyle="light-content"/>
                     <View style={styles.layout}>
+
                         <TouchableOpacity
                             onPress = {this.props.onBack}>
                             <Image style={styles.back}
@@ -43,14 +47,21 @@ export default class Header extends Component {
 }
 
 const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
-const APPBAR_HEIGHT = Platform.OS === 'ios' ? 64 : 56;
+const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
 
 const styles = StyleSheet.create({
     container: {
         alignSelf:'stretch',
-        marginTop: STATUSBAR_HEIGHT,
-        height: APPBAR_HEIGHT
+        marginTop: 0,
+        height: APPBAR_HEIGHT + STATUSBAR_HEIGHT
     },
+    statusBar: {
+        height: STATUSBAR_HEIGHT,
+      },
+      appBar: {
+        backgroundColor:'#79B45D',
+        height: APPBAR_HEIGHT,
+      },
     layout:{
         flex:1,
         paddingLeft:10,
