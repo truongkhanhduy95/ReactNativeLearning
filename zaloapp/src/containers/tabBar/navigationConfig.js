0f2bf4ac8a733428ca218e9521e0e32cbef4b3f6
duy_truong <truongkhanhduy9@gmail.com>
2018-05-10 17:42:58 +0700

    Update tab icon

--- zaloapp/src/containers/tabBar/navigationConfig.js
+++ zaloapp/src/containers/tabBar/navigationConfig.js
@@ -33,10 +33,10 @@ const tabbarConfiguration = {
         showIcon: true,
         showLabel: true,
         style:{
-            height: 50
+            height: 56
         },
         labelStyle: {
-            fontSize: 12,
+            fontSize: 11,
             marginBottom: 4,
         },
         iconStyle: {
